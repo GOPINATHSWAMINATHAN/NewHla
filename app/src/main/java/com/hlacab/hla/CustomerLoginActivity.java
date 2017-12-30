67ebf11d3ea72f1fe935450aa0a352524e120d0f
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2017-12-30 12:20:19 +0300

    Second Commit

--- app/src/main/java/com/hlacab/hla/CustomerLoginActivity.java
+++ app/src/main/java/com/hlacab/hla/CustomerLoginActivity.java
@@ -1,15 +1,23 @@
 package com.hlacab.hla;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -17,14 +25,25 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerLoginActivity extends AppCompatActivity {
 
-    private EditText mEmail, mPassword;
+    private EditText mEmail, mPassword, mName, mPhone;
     private Button mLogin, mRegistration;
-
+    private ImageView mProfileImage;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    Uri resultUri;
+    TextView signInText, registerText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +56,7 @@ public class CustomerLoginActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if(user!=null){
+                if (user != null) {
                     Intent intent = new Intent(CustomerLoginActivity.this, CustomerMapActivity.class);
                     startActivity(intent);
                     finish();
@@ -48,24 +67,112 @@ public class CustomerLoginActivity extends AppCompatActivity {
 
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
-
+        mName = findViewById(R.id.name);
+        mPhone = findViewById(R.id.phone);
         mLogin = (Button) findViewById(R.id.login);
+        signInText = findViewById(R.id.sign_in_text);
+        registerText = findViewById(R.id.register_text);
+        registerText.setVisibility(View.GONE);
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProfileImage.setVisibility(View.VISIBLE);
+                signInText.setVisibility(View.VISIBLE);
+                mRegistration.setVisibility(View.GONE);
+                mName.setVisibility(View.VISIBLE);
+                mPhone.setVisibility(View.VISIBLE);
+                registerText.setVisibility(View.INVISIBLE);
+
+            }
+        });
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProfileImage.setVisibility(View.GONE);
+                mLogin.setVisibility(View.VISIBLE);
+                mRegistration.setVisibility(View.GONE);
+                registerText.setVisibility(View.VISIBLE);
+                signInText.setVisibility(View.GONE);
+                mName.setVisibility(View.GONE);
+                mPhone.setVisibility(View.GONE);
+            }
+        });
+
         mRegistration = (Button) findViewById(R.id.registration);
 
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
         mRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
+                final String name = mName.getText().toString();
+                final String phone = mPhone.getText().toString();
+
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
+                        if (!task.isSuccessful()) {
                             Toast.makeText(CustomerLoginActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
-                        }else{
+                        } else {
                             String user_id = mAuth.getCurrentUser().getUid();
-                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
+                            final DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
                             current_user_db.setValue(true);
+                            Map userInfo = new HashMap();
+                            userInfo.put("name", name);
+                            userInfo.put("phone", phone);
+                            userInfo.put("email", email);
+                            userInfo.put("password", password);
+                            current_user_db.updateChildren(userInfo);
+
+                            if (resultUri != null) {
+
+                                StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(user_id);
+                                Bitmap bitmap = null;
+                                try {
+                                    bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+                                byte[] data = baos.toByteArray();
+                                UploadTask uploadTask = filePath.putBytes(data);
+
+                                uploadTask.addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        finish();
+                                        return;
+                                    }
+                                });
+                                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                                        Map newImage = new HashMap();
+                                        newImage.put("profileImageUrl", downloadUrl.toString());
+                                        current_user_db.updateChildren(newImage);
+
+                                        finish();
+                                        return;
+                                    }
+                                });
+                            } else {
+                                finish();
+                            }
+
+
                         }
                     }
                 });
@@ -80,7 +187,7 @@ public class CustomerLoginActivity extends AppCompatActivity {
                 mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
+                        if (!task.isSuccessful()) {
                             Toast.makeText(CustomerLoginActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -96,9 +203,20 @@ public class CustomerLoginActivity extends AppCompatActivity {
         super.onStart();
         mAuth.addAuthStateListener(firebaseAuthListener);
     }
+
     @Override
     protected void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
     }
 }
